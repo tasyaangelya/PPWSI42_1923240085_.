db0023ff2e7f23f4211d6d2442c001e8c21e8d9c
tasyaangelya <tasyaangelya12@mhs.mdp.ac.id@domain.com>
2021-06-13 12:08:14 +0700

    Belajar Migration dan membuat controller Mahasiswa

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function insert()
+    {
+ 
+    }
+ 
+    public function update()
+    {
+ 
+    }
+ 
+    public function delete()
+    {
+ 
+    }
+ 
+    public function select()
+    {
+ 
+    }
+}
